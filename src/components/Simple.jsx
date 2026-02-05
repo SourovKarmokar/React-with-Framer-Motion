8f88cd8064b08b17f6450ba727c6d339be6e595d
SourovKarmokar <sourovkarmokar22@gmail.com>
2026-02-06 01:57:29 +0600

    Motion

--- src/components/Simple.jsx
+++ src/components/Simple.jsx
@@ -1,8 +1,27 @@
-import { motion } from "motion/react"
+import { motion } from "motion/react";
 
 const Simple = () => {
   return (
-    <motion.ul animate={{ rotate: 360 }} />
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <motion.div
+        animate={{
+          scale: [1, 2, 2, 1, 1],
+          rotate: [0, 0, 270, 270, 0],
+          borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+        }}
+        transition={{
+          duration: 2,
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatDelay: 1,
+        }}
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: "#00d1ff",
+        }}
+      />
+    </div>
   );
 };
 
